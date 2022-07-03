a2565bd5d0cd4d91c4cfb1943daa642345288a7b
Brendon DSouza <brendonmufc@gmail.com>
2022-07-03 10:23:57 -0400

    added production build

---
+++ build/precache-manifest.2a116d748136eed200deda2c465cb82c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95fafd8e61ceaab413f5b081b6e828f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb45dfe1288749115e3",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb45dfe1288749115e3",
+    "url": "/static/js/main.95764464.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
